0acbab2d140e6f83103db37102969d8fa8877eab
Khalid Suleymanov <167674232+KhalidSuleymanov@users.noreply.github.com>
2024-04-21 16:42:30 +0400

    Add files via upload

---
+++ DateTime/DateTime/Group.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTime
+{
+    internal class Group
+    {
+        public string No;
+        public GroupType Type;
+        public DateTime StartDate;
+    }
+}
---
+++ DateTime/DateTime/GroupType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTime
+{
+    internal enum GroupType : byte
+    {
+        Programming = 1,
+        Design = 2,
+        System = 3,
+    }
+}
